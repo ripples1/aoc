682822fa8708e7ebb5c6c25b79aecff9f97c44ca
Vladislav Kolesnik <vkolesnik@boston-unisoft.com>
2024-02-20 17:51:28 +0200

    Add day 09

---
+++ challenges/src/_2023/d09.rs
@@ -0,0 +1,35 @@
+// Here goes
+
+fn parse_number_list(s: &str) -> Vec<i64> {
+  s.split(" ").map(|n| n.parse().unwrap()).collect()
+}
+
+fn is_all_zero(history: &[i64]) -> bool {
+  history.iter().all(|n| *n == 0)
+}
+
+fn diff(history: &[i64]) -> Vec<i64> {
+  history.windows(2).map(|w| w[1] - w[0]).collect()
+}
+
+fn next_item(history: &[i64]) -> i64 {
+  if is_all_zero(history) {
+    return 0;
+  }
+  history.last().unwrap() + next_item(diff(history).as_slice())
+}
+
+fn prev_item(history: &[i64]) -> i64 {
+  if is_all_zero(history) {
+    return 0;
+  }
+  history.first().unwrap() - prev_item(diff(history).as_slice())
+}
+
+pub fn part_1(input: &str) -> i64 {
+  input.lines().map(parse_number_list).map(|h| next_item(h.as_slice())).sum()
+}
+
+pub fn part_2(input: &str) -> i64 {
+  input.lines().map(parse_number_list).map(|h| prev_item(h.as_slice())).sum()
+}
--- challenges/src/lib.rs
+++ challenges/src/lib.rs
@@ -19,6 +19,8 @@ pub fn run(input: &str, challenge: &str) -> i64 {
     "2023/7/2" => _2023::d07::part_2(input),
     "2023/8/1" => _2023::d08::part_1(input),
     "2023/8/2" => _2023::d08::part_2(input),
+    "2023/9/1" => _2023::d09::part_1(input),
+    "2023/9/2" => _2023::d09::part_2(input),
     _ => panic!("Bad challenge path")
   }
 }
